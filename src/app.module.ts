852a2979e9040a27dccdf2f8219f6711c8de48dc
Joao Rodrigues <joao.rodrigues@consensys.net>
2020-01-10 12:11:49 +0000

    Add configuration module to load .env properties

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CryptographyModule } from './cryptography/cryptography.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [CryptographyModule],
+  imports: [ConfigModule.forRoot({
+    load: [configuration],
+  }), CryptographyModule],
   controllers: [],
   providers: [],
 })
