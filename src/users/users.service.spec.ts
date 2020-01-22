6961cf223b3d0220c0fe97d4d85c596c117abbf6
sproj <alan.jones@consensys.net>
2020-01-22 18:55:01 +0000

    unit test users service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { mockRepository } from '../../test/test-helpers';
+import { CryptographyService } from '../cryptography/cryptography.service';
+import CreateUserDto from './dto/create-user.dto';
+import { User } from './entities/users.entity';
 import { UsersService } from './users.service';
 
-xdescribe('UsersService', () => {
+describe('UsersService', () => {
   let service: UsersService;
+  let repository: Repository<User>;
+
+  const anonName = 'anon';
+  const anonUserDto: CreateUserDto = { name: anonName };
+  const anonUser = new User();
+  anonUser.name = anonName;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, CryptographyService, { provide: getRepositoryToken(User), useClass: mockRepository }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('calls user repository to save a user', async () => {
+    await service.create(anonUserDto);
+
+    expect(repository.save).toBeCalledTimes(1);
+  });
+
+  it('calls user repository to find a user', async () => {
+    jest.spyOn(repository, 'findOne').mockResolvedValueOnce(anonUser);
+
+    const actual = await service.find(anonName);
+
+    expect(repository.findOne).lastCalledWith({ where: { name: anonName } });
+    expect(actual).toEqual(anonUser);
+  });
+
+  it('calls user repository to find a user by name', async () => {
+    jest.spyOn(repository, 'findOne').mockResolvedValueOnce(anonUser);
+
+    const actual = await service.findByName(anonName);
+
+    expect(repository.findOne).lastCalledWith({ select: ['name'], where: { name: anonName } });
+    expect(actual).toEqual(anonUser);
+  });
+
+  it('calls user repository to find a user by uuid', async () => {
+    jest.spyOn(repository, 'findOneOrFail').mockResolvedValueOnce(anonUser);
+
+    const actual = await service.findByUuid(anonUser.uuid);
+
+    expect(repository.findOneOrFail).lastCalledWith({ where: { uuid: anonUser.uuid } });
+    expect(actual).toEqual(anonUser);
+  });
+
+  it('calls user repository to delete a user by name', async () => {
+    await service.delete(anonName);
+
+    expect(repository.delete).lastCalledWith({ name: anonName });
   });
 });
