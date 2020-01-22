d89fb8b10c426d781cace59b916ef658c227415c
sproj <alan.jones@consensys.net>
2020-01-22 18:54:43 +0000

    add common test helper for mocking repositories

---
+++ test/test-helpers.ts
@@ -0,0 +1,16 @@
+/**
+ * A jest.Mock of a repository so Providers or modules which depend on them can be unit-tested.
+ * Implementation of methods exposed here can be mocked in place.
+ */
+export const mockRepository = jest.fn(() => {
+  return {
+    metadata: {
+      columns: [],
+      relations: [],
+    },
+    save: jest.fn(),
+    findOne: jest.fn(),
+    findOneOrFail: jest.fn(),
+    delete: jest.fn(),
+  };
+});
