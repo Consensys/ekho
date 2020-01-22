0d99ba2c7c661785436e2b378b978ca0d2a7ed65
sproj <alan.jones@consensys.net>
2020-01-22 20:01:10 +0000

    unit test events controller

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { mockRepository } from '../../test/test-helpers';
 import { EventsController } from './events.controller';
+import { EkhoEvent } from './events.entity';
+import { EventsService } from './events.service';
 
-xdescribe('Events Controller', () => {
+describe('Events Controller', () => {
   let controller: EventsController;
+  let service: EventsService;
+
+  const anonEvent: EkhoEvent = {
+    id: -1,
+    txHash: '0x123',
+    status: 'DERP',
+    createdDate: new Date(0),
+    channelId: 'ANON_CHANNELID',
+    content: 'lalala',
+    signature: 'Made in Ireland',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [{ provide: getRepositoryToken(EkhoEvent), useClass: mockRepository }, EventsService],
       controllers: [EventsController],
     }).compile();
 
+    service = module.get<EventsService>(EventsService);
     controller = module.get<EventsController>(EventsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('calls EventsService.getAll to get events', async () => {
+    jest.spyOn(service, 'getAll').mockResolvedValueOnce([anonEvent]);
+
+    const [actual] = await controller.get();
+
+    expect(service.getAll).toBeCalledTimes(1);
+    expect(actual).toBe(anonEvent);
   });
 });
