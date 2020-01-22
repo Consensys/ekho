b29075ed15e0db7922271ca37360912eb9e6e9eb
sproj <alan.jones@consensys.net>
2020-01-22 17:27:04 +0000

    document test-helpers

--- src/cryptography/test-helpers/cryptography.test-helpers.ts
+++ src/cryptography/test-helpers/cryptography.test-helpers.ts
@@ -33,7 +33,16 @@ export interface TestHelper {
   ): Promise<SignedSharedSecret>;
 }
 
+/**
+ * Fetch a TestHelper for the CryptographyService.
+ * @param service CryptographyService instance
+ * @returns Object with test helper methods, which themselves wrap cryptography-service methods.
+ */
 export const getTestHelper = (service: CryptographyService): TestHelper => {
+  /**
+   * Generate an anonymous signing pair, and one-time pair.
+   * @returns Object with signingPair and oneTimePair. Each being a CryptographyPairDto
+   */
   const generateAnonKeys = async (): Promise<AnonKeySet> => {
     const signingPair = await service.generateSigningKeyPair();
     const oneTimePair = await service.generateOneUseKeyPair();
@@ -44,6 +53,10 @@ export const getTestHelper = (service: CryptographyService): TestHelper => {
     };
   };
 
+  /**
+   * Generate TestSubjects alice, bob and trudy, each with a distince signing pair and one-time pair and an undefined sharedSecret property.
+   * @returns Object with properties alice, bob and trudy. Each a TestSubject.
+   */
   const generateAlicenBob = async (): Promise<TestUsers> => {
     const alice = await generateAnonKeys();
     const bob = await generateAnonKeys();
@@ -55,6 +68,13 @@ export const getTestHelper = (service: CryptographyService): TestHelper => {
     };
   };
 
+  /**
+   * Generate a shared secret and its signature
+   * @param yourPublic1time A 32 byte buffer containing 'my public one-time key..
+   * @param myPriv1time A 32 byte buffer containing 'your' private one-time key.
+   * @param myPrivSigning A 32 byte buffer containing 'my' private signing key.
+   * @returns Object with properties secret, and signature, being 'my' shared secret with 'you' and my signature of that secret. Each a Buffer.
+   */
   const generateSharedSecret = async (
     yourPublic1time: Buffer,
     myPriv1time: Buffer,
