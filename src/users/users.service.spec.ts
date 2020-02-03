9f2cf4c9121452a779149ecc88de6096b9704645
sproj <sproj@users.noreply.github.com>
2020-02-03 14:24:18 +0000

    correct branch on findById + cover (#24)

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -29,6 +29,12 @@ describe('UsersService', () => {
     expect(service).toBeDefined();
   });
 
+  it('userRepository should be defined', async () => {
+    expect(repository).toBeDefined();
+    expect(repository.findOne).toBeDefined();
+    expect(repository.findOneOrFail).toBeDefined();
+  });
+
   it('calls user repository to save a user', async () => {
     jest.spyOn(repository, 'save').mockResolvedValueOnce(anonUser);
 
@@ -52,13 +58,37 @@ describe('UsersService', () => {
 
     const actual = await service.findByName(anonName);
 
+    expect(repository.findOne).toBeCalledTimes(1);
     expect(repository.findOne).lastCalledWith({ select: ['name'], where: { name: anonName } });
     expect(actual).toEqual(anonUser);
   });
 
   it('calls user repository to delete a user by name', async () => {
+    jest.spyOn(repository, 'delete').mockResolvedValueOnce(undefined);
+
     await service.delete(anonName);
 
+    expect(repository.delete).toBeCalledTimes(1);
     expect(repository.delete).lastCalledWith({ name: anonName });
+  });
+
+  it('findById delegates userRepository to find a user by a given id', async () => {
+    jest.spyOn(repository, 'findOne').mockResolvedValueOnce(anonUser);
+
+    const actual = await service.findById(anonUser.id);
+
+    expect(repository.findOne).toBeCalledTimes(1);
+    expect(repository.findOne).lastCalledWith(anonUser.id);
+    expect(actual).toBe(anonUser);
+  });
+
+  it('findById has an optional boolean `orFail` param, using which means delegating findOrFail rather than findOne', async () => {
+    jest.spyOn(repository, 'findOneOrFail').mockResolvedValueOnce(anonUser);
+
+    const actual = await service.findById(anonUser.id, true);
+
+    expect(repository.findOneOrFail).toBeCalledTimes(1);
+    expect(repository.findOneOrFail).lastCalledWith(anonUser.id);
+    expect(actual).toBe(anonUser);
   });
 });
